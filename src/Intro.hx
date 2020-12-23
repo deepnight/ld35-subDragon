8146fa7d5cd6f33b2d8d7b0361e1baa3cfc34b3d
Sébastien Bénard <github@deepnight.net>
2020-12-23 12:58:47 +0100

    Added loading message on startup Better intro

--- src/Intro.hx
+++ src/Intro.hx
@@ -8,14 +8,26 @@ class Intro extends dn.Process {
 
 		var ca = Main.ME.controller.createAccess("intro");
 		logo = Assets.tiles.h_get("logo",0, 0.5,0.5, root);
-		// logo.setScale( M.fmax( wid/logo.tile.width, hei/logo.tile.height ) );
-		tw.createMs(logo.alpha, 0>1, 1500);
+
+		// var mul = #if debug 0.3 #else 1 #end;
+		var mul = 1;
+		tw.createMs(logo.alpha, 0>1, 1500*mul);
 		delayer.addMs( function() {
-			tw.createMs(logo.alpha,0, 1500).end( function() {
-				new Game();
-				destroy();
+			var tf = new h2d.Text(Assets.font, root);
+			tf.text = "Initialiazing level...";
+			tf.x = Std.int( w()/Const.UPSCALE*0.5 - tf.textWidth*0.5*tf.scaleX );
+			tf.y = Std.int( h()/Const.UPSCALE*0.9 - tf.textHeight*tf.scaleY );
+			tw.createMs(logo.alpha,0.7, 700).end( function() {
+				delayer.addF(()->{
+					new Game();
+					root.parent.addChild(root);
+				}, 1);
+				delayer.addF(()->{
+					tw.createMs(tf.alpha, 0, 500);
+					tw.createMs(logo.alpha, 0, 1500).end(destroy);
+				}, 2);
 			});
-		}, 3800);
+		}, 1900*mul);
 
 		onResize();
 	}
